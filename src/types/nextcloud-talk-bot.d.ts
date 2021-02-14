b71cf3d8ef136919133ce7f76c00760eed143d19
CaptaNova <captanova@christoph-noelke.de>
2021-02-14 19:51:05 +0100

    chore: create typings for nextcloud-talk-bot

---
+++ src/types/nextcloud-talk-bot.d.ts
@@ -0,0 +1,36 @@
+declare module "nextcloud-talk-bot" {
+  class NextcloudTalkBot {
+    user: string;
+
+    constructor(options = {});
+
+    on(event: "message" | "system", listener: (message: any) => void);
+
+    getChannel(channelId: string, join?: boolean): any;
+
+    inChannel(channelId: string): boolean;
+
+    joinChannel(channelId: string, name?: string): boolean;
+
+    onText(
+      regxt: RegExp | string,
+      callback: Function,
+      channelId?: string
+    ): this;
+
+    sendText(text: string, channelId: string, messageId?: number): fetch;
+
+    startPolling(isAutoJoinRequest?: boolean): void;
+
+    _request(
+      path: string,
+      opt = {}
+    ): Promise<
+      | { type: "error"; response: any }
+      | { type: "response"; data: any; headers: any }
+      | { type: "unchanged" }
+    >;
+  }
+
+  export = NextcloudTalkBot;
+}
